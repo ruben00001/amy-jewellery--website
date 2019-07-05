135941226fa766d9f8a54c23046d4cee725b52a0
unknown <a.ruben00001@gmail.com>
2019-07-05 11:36:44 +0100

    change insta link

--- src/components/contact.js
+++ src/components/contact.js
@@ -5,7 +5,7 @@ const Contact = () => (
     <img className="contact-img" src={require("../img/22118947_10159649787355647_624928218_n (1).jpg")} alt="jewellery" />
     <div className="container--contact-details">
         <p>amy@amyrodriguez.co.uk</p>
-        <a target="__blank" href="https://www.instagram.com/amy_rodriguez___/">
+        <a target="__blank" href="https://www.instagram.com/amyrodriguezjewellery/">
             <p>@amy_rodriguez___</p>
         </a>
     </div>
