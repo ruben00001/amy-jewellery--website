2a6adb423af47e3c431486ea7d60d2553cf605f2
unknown <a.ruben00001@gmail.com>
2019-07-05 12:31:05 +0100

    add insta logo

--- src/components/contact.js
+++ src/components/contact.js
@@ -4,9 +4,9 @@ const Contact = () => (
   <div className="container--contact">
     <img className="contact-img" src={require("../img/22118947_10159649787355647_624928218_n (1).jpg")} alt="jewellery" />
     <div className="container--contact-details">
-        <p>amy@amyrodriguez.co.uk</p>
-        <a target="__blank" href="https://www.instagram.com/amyrodriguezjewellery/">
-            <p>@amy_rodriguez___</p>
+        <p className="container--contact-details_email">amy@amyrodriguez.co.uk</p>
+        <a className="container--contact-details_insta" target="__blank" href="https://www.instagram.com/amyrodriguezjewellery/">
+            <img src={require("../img/IG_Glyph_Fill.png")}></img>
         </a>
     </div>
   </div>
